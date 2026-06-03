20019cf7ff9b1b26a34430cdae45bd5630e60cff
zahrazefania <zahrazefania1101@student.uns.ac.id>
2026-06-03 11:49:21 +0700

    coba

--- HEWAN.py
+++ HEWAN.py
@@ -1,4 +1,26 @@
 <<<<<<< HEAD
+<<<<<<< Updated upstream
+=======
+from abc import ABC, abstractmethod
+
+class Hewan(ABC):
+    def __init__(self, nama, jenis):
+        self_nama=nama
+        self_jenis=jenis
+        
+    @abstractmethod    
+    def makan(self):
+        pass
+    
+class HewanTerbang(ABC):
+    @abstractmethod
+    def terbang(self):
+        
+    
+    
+=======
+<<<<<<< HEAD
+>>>>>>> Stashed changes
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -80,4 +102,9 @@ class HewanTerbang(ABC):
     @abstractmethod
     def terbang(self):
 >>>>>>> 87cfe479a4491ebe42f671df143f133f7ae79782
+<<<<<<< Updated upstream
         pass
+=======
+        pass
+>>>>>>> d1640bcac75f649a497096b300902e8985b6b191
+>>>>>>> Stashed changes
