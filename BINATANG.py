48addc4d31cde9df7dd1f5d4103fc3b72a6828c0
udzmanufuus0702-bit <udzmanufuus0702@gmail.com>
2026-06-03 11:04:34 +0700

    Add files via upload

---
+++ BINATANG.py
@@ -0,0 +1,14 @@
+from HEWAN import Hewan, HewanTerbang
+
+class Burung(HewanTerbang):
+    
+    def makan(self):
+        print(f"{self.nama} sedang makan biji-bijian.")
+        
+    def terbang(self):
+        print(f"{self.nama} sedang terbang di udara.")
+
+class Ikan(Hewan):
+    def makan(self):
+        print(f"{self.nama} sedang makan pelet ikan.")
+        
